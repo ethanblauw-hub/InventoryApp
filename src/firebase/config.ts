b165c7fabb31dd34f1651729ecfef9b78438c692
Ethan Blauw <edb@eganco.com>
2025-10-07 15:28:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1025404824-b0cf8",
+  "appId": "1:394731799727:web:8839bee90c79b248bfc05a",
+  "apiKey": "AIzaSyAE_7BU9x8nzrmBSdnl2iVylbujQDrZsY4",
+  "authDomain": "studio-1025404824-b0cf8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "394731799727"
+};
